cda92132d972af2334350c8369ad0896b692f0b2
ayoubensalem <ayoubensalem@gmail.com>
2018-02-13 07:58:48 +0000

    setup file

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+with open("README.rst", "r") as f:
+    readme = f.read()
+
+setup(
+    name="jyml-tool",
+    version="0.1.0",
+    description="Jenkins YAML tool to convert jobs and create them.",
+    long_description=readme,
+    author='Ayoub ED-DAFALI',
+    author_email='ayoubensalem@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'':'src'},
+    install_requires=[],
+    entry_points={
+            'console_scripts': [
+                'jyml=jyml.cli:main'],
+        }
+)
+
